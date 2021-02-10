0042f13ef6d5596d84c3f0ab1724cf0abd855eaf
Masterlau Lau <Masterlau2@hotmail.com>
2021-02-09 22:29:24 -0500

    Base for implementing cpu handling

--- THREEjs Port/main.js
+++ THREEjs Port/main.js
@@ -1,61 +1,112 @@
-// GLSL
-const vShader = `
-varying vec2 v_uv;
+const _maxSamplesSize = 1020;
+let nbSamples = _maxSamplesSize;
 
-void main() {
-  v_uv = uv;
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-}
-`;
-const fShader = `
-varying vec2 v_uv;
-uniform vec2 u_mouse;
-uniform vec2 u_resolution;
-uniform float u_time;
-uniform float samplesSize;
-uniform float samples[1020];
+let redererWidth = window.innerWidth;
+let redererHeight = window.innerHeight;
 
-void main() {
-  vec2 v = u_mouse / u_resolution;
-  vec2 uv = gl_FragCoord.xy / u_resolution;
-
-  float index = uv.y * samplesSize;
-
-  int clampIndex = 0;                                       //int(clamp(index, 0.0,  1019.0));
-  float sampleValue = abs(samples[0]);
-
-  float pixelValue = 0.0;
-  if ((0.5 - sampleValue / 2.0 < uv.x) && (0.5 + sampleValue / 2.0 > uv.x))
-  {
-    pixelValue = 1.0;
-  }
-  
-  gl_FragColor = vec4(pixelValue, uv.y, 1.0, 1.0).rgba;
-}
-`;
-
-const _samplesSize = 1020;
-const nbSamples = _samplesSize;
-
-// Random fucking samples 
-let samples = new Float32Array(_samplesSize);
-for (var i= 0; i < nbSamples; i++)
+function GetRandomSample()
 {
-    samples[i] = (Math.random() * 2.0 - 1.0); // Normalize random [-1, 1]
+    var samples = new Float32Array(_maxSamplesSize);
+    for (var i= 0; i < nbSamples; i++)
+    {
+        samples[i] = i / nbSamples;//(Math.random() * 2.0 - 1.0); // Normalize random [-1, 1]
+
+        // Temp
+        if (i == 0)
+        {
+            samples[i] = 0.5;
+        }
+        if (i == nbSamples -1)
+        {
+            samples[i] = 0.5;
+        }
+    }
+    return samples; 
+}
+
+function GetSamplesResolution()
+{
+    if (redererHeight > _maxSamplesSize)
+    {
+        return _maxSamplesSize;
+    }
+    return redererHeight;
 }
 
 let scene;
 let camera;
 let render;
+
 // shader uniforms
 const uniforms = {
-    u_mouse: { value: { x: window.innerWidth / 2, y: window.innerHeight / 2 } },
-    u_resolution: { value: { x: window.innerWidth, y: window.innerHeight } },
+    u_mouse: { value: { x: redererWidth / 2, y: redererHeight / 2 } },
+    u_resolution: { value: { x: redererWidth, y: redererHeight } },
     u_time: { value: 0.0 },
-    samplesSize: { value: _samplesSize },
-    samples: { type: "fv", value: samples}
+    samples: { type: "fv", value: GetRandomSample()}
   }
 const clock = new THREE.Clock();
+
+
+let plane;
+function UpdateRenderPlane()
+{
+    scene.remove(plane);
+
+    var resolution = GetSamplesResolution();
+
+    // GLSL
+    var vertexShader = `
+    varying vec2 v_uv;
+
+    void main() {
+    v_uv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+    `;
+
+    var fragmentShader = `
+    varying vec2 v_uv;
+    uniform vec2 u_mouse;
+    uniform vec2 u_resolution;
+    uniform float u_time;
+    uniform float samples[${resolution}];
+
+    void main() {
+    vec2 v = u_mouse / u_resolution;
+    vec2 uv = gl_FragCoord.xy / u_resolution;
+
+    int index = int((1.0 - uv.y) * ${resolution}.0);
+
+    float sampleValue = 0.0;
+    for (int i=0; i < ${resolution}; i++)
+    {
+        if(index == i)
+        {
+            sampleValue = abs(samples[i]);
+        }
+    }
+
+    float pixelValue = 0.0;
+    if ((0.5 - sampleValue / 2.0 < uv.x) && (0.5 + sampleValue / 2.0 > uv.x))
+    {
+        pixelValue = 1.0;
+    }
+
+    gl_FragColor = vec4(pixelValue, pixelValue, pixelValue, 1.0).rgba;
+    }
+    `;
+
+    // Plane (To draw waveform)
+    const geometry = new THREE.PlaneGeometry();
+    const material = new THREE.ShaderMaterial({
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader,
+        uniforms
+      });
+    plane = new THREE.Mesh( geometry, material );
+    scene.add( plane );
+}
+
 
 function init()
 {
@@ -64,8 +115,8 @@ function init()
 
     // Camera
     camera = new THREE.PerspectiveCamera(
-        54,
-        window.innerWidth / window.innerHeight,
+        10,
+        redererWidth / redererHeight,
         0.1,
         1000
     );
@@ -73,18 +124,10 @@ function init()
 
     // Renderer
     renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(redererWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    // Plane (To draw waveform)
-    const geometry = new THREE.PlaneGeometry();
-    const material = new THREE.ShaderMaterial({
-        vertexShader: vShader,
-        fragmentShader: fShader,
-        uniforms
-      });
-    const plane = new THREE.Mesh( geometry, material );
-    scene.add( plane );
+    UpdateRenderPlane();
 }
 
 // Render loop
@@ -95,17 +138,24 @@ function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
+
 // Rezise handling
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
+    redererWidth = window.innerWidth;
+    redererHeight = window.innerHeight;
+
+    camera.aspect = redererWidth / redererHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(redererWidth, redererHeight);
 
     if (uniforms.u_resolution !== undefined){
-        uniforms.u_resolution.value.x = window.innerWidth;
-        uniforms.u_resolution.value.y = window.innerHeight;
-      }
+        uniforms.u_resolution.value.x = redererWidth;
+        uniforms.u_resolution.value.y = redererHeight;
+    }
+
+    UpdateRenderPlane();
 }
+
 // Mouse handling 
 document.addEventListener('mousemove', (e) =>{
     window.addEventListener( 'resize', onWindowResize, false );
